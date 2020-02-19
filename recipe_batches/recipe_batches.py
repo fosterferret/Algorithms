942609ea6954ce1bd4db33b55e1b2bbed073a047
Francis Bulus <notchera@gmail.com>
2020-02-19 15:14:21 +0100

    completed recipes challenge

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,22 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    if set(recipe.keys()).issubset(set(ingredients.keys())):
+        batches, max_batches = 0, math.inf
+        for required_ingredient in recipe:
+            batches = ingredients[required_ingredient] // recipe[required_ingredient]
+            max_batches = batches if batches < max_batches else max_batches
+        return max_batches
+    else:
+      return 0
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,26 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    min_price_so_far = prices[0]
+    max_profit_so_far = prices[1] - min_price_so_far
+    for i in range(len(prices) - 1):
+        min_price_so_far = prices[i] if prices[i] < min_price_so_far \
+            else min_price_so_far
+        current_profit = prices[i + 1] - min_price_so_far
+        max_profit_so_far = current_profit if current_profit > max_profit_so_far \
+            else max_profit_so_far
+    return max_profit_so_far
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
